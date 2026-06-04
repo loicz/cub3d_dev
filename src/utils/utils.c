d87fff7d4e03077e9f003808459dc318f010a9d3
lozhao <lozhao@student.42.fr>
2026-06-04 15:18:17 +0200

    added utils.c and some functions

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,35 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_scene.c                                      :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lozhao <lozhao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/02 14:56:47 by lozhao            #+#    #+#             */
-/*   Updated: 2026/06/04 15:16:58 by lozhao           ###   ########.fr       */
+/*   Created: 2026/06/04 12:27:04 by lozhao            #+#    #+#             */
+/*   Updated: 2026/06/04 15:17:04 by lozhao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include "libft.h"
 
-static int	is_dotcub(char *path)
+int	err_msg(char *s)
 {
-	int	len;
-
-	len = ft_strlen(path);
-	if (len <= 4)
-		return (0);
-	return (ft_strncmp(path + len - 4, ".cub", 4) == 0);
-}
-
-int	parse_scene(char *path, t_game *game)
-{
-	int	fd;
-
-	if (!is_dotcub(path))
-		return (err_msg("scene file must end with .cub"));
-	fd = open(path, O_RDONLY);
-	if (fd < 0)
-		return (err_msg("cannot open scene file"));
-
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(s, 2);
+	ft_putstr_fd("\n", 2);
+	return (1);
 }
