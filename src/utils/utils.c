a70661e5c90a854de7f3e31d30a0151d940d50cd
lozhao <lozhao@student.42.fr>
2026-06-09 23:05:55 +0200

    fixed free fonctions and modified data structure for mlx

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: tle-rhun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 12:27:04 by lozhao            #+#    #+#             */
-/*   Updated: 2026/06/09 21:11:26 by lozhao           ###   ########.fr       */
+/*   Updated: 2026/06/09 23:00:51 by lozhao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,57 @@ int	err_msg(char *s)
 	return (1);
 }
 
+static void	destroy_img(t_mlx *mlx, t_img *img)
+{
+	if (!mlx || !mlx->ptr || !img || !img->img)
+		return ;
+	mlx_destroy_image(mlx->ptr, img->img);
+	img->img = NULL;
+	img->addr = NULL;
+}
+
+static void	destroy_textures(t_mlx *mlx)
+{
+	int	i;
+
+	if (!mlx)
+		return ;
+	i = 0;
+	while (i < 4)
+	{
+		destroy_img(mlx, &mlx->tex[i]);
+		i++;
+	}
+}
+
+void	destroy_mlx(t_game *game)
+{
+	t_mlx	*mlx;
+
+	if (!game)
+		return ;
+	mlx = &game->mlx;
+	if (!mlx->ptr)
+		return ;
+	if (mlx->win)
+	{
+		mlx_destroy_window(mlx->ptr, mlx->win);
+		mlx->win = NULL;
+	}
+	destroy_textures(mlx);
+	destroy_img(mlx, &mlx->frame);
+	mlx_destroy_display(mlx->ptr);
+	free(mlx->ptr);
+	mlx->ptr = NULL;
+}
+
+int	on_destroy(t_game *game)
+{
+	destroy_mlx(game);
+	free_parser_data(game);
+	exit(0);
+	return (0);
+}
 // void	close_window(t_game *game, t_img img)
 // {
 // 	mlx_destroy_image(img.ptr, img.img);
@@ -28,7 +79,7 @@ int	err_msg(char *s)
 // 	mlx_destroy_display(img.ptr);
 // 	free(img.ptr);
 // }
-
+//
 // void	exit_cub3d(t_game *game)
 // {
 // 	close_window(game, game->frame);
