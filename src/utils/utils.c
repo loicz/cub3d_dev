13aaa9b9639b0dc3a8272880cb76fe140dbc963e
lozhao <lozhao@student.42.fr>
2026-06-09 21:18:26 +0200

    commented utils functions waiting to fix

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,12 +6,11 @@
 /*   By: tle-rhun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 12:27:04 by lozhao            #+#    #+#             */
-/*   Updated: 2026/06/09 18:13:57 by tle-rhun         ###   ########.fr       */
+/*   Updated: 2026/06/09 21:11:26 by lozhao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-#include "libft.h"
 
 int	err_msg(char *s)
 {
@@ -21,18 +20,18 @@ int	err_msg(char *s)
 	return (1);
 }
 
-void	close_window(t_game *game, t_img img)
-{
-	mlx_destroy_image(img.ptr, img.img);
-	mlx_destroy_window((img.ptr), img.win);
-	mlx_loop_end(img.ptr);
-	mlx_destroy_display(img.ptr);
-	free(img.ptr);
-}
+// void	close_window(t_game *game, t_img img)
+// {
+// 	mlx_destroy_image(img.ptr, img.img);
+// 	mlx_destroy_window((img.ptr), img.win);
+// 	mlx_loop_end(img.ptr);
+// 	mlx_destroy_display(img.ptr);
+// 	free(img.ptr);
+// }
 
-void	exit_cub3d(t_game *game)
-{
-	close_window(game, game->frame);
-	free_parser_data(game);
-	exit(1);
-}
+// void	exit_cub3d(t_game *game)
+// {
+// 	close_window(game, game->frame);
+// 	free_parser_data(game);
+// 	exit(1);
+// }
