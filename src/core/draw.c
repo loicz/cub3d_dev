c96ffdb0a81fb63a1748643feca2cbd7fe8b56fe
tle-rhun <tle-rhun@f4r10s2.paris.42.school>
2026-06-11 15:30:44 +0200

    function draw_vertical_line has 25 line

--- src/core/draw.c
+++ src/core/draw.c
@@ -6,7 +6,7 @@
 /*   By: tle-rhun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 17:53:12 by tle-rhun          #+#    #+#             */
-/*   Updated: 2026/06/11 14:43:00 by tle-rhun         ###   ########.fr       */
+/*   Updated: 2026/06/11 15:29:57 by tle-rhun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,39 +32,32 @@ int	color_texture(t_img img, int x, int i, int distance)
 	// printf("x:%d\n", x);
 	// printf("img.width:%d\n", img.width);
 	color = img.addr[get_index(x % img.width, y, img.line_len)];
-	// if (x > 350 && x < 370)
-	// printf("index:%d x:%d\n", get_index(x % img.width, y, img.line_len), x
 	// % img.width);
 	// if (x == 0)
 	// printf("index0:%d\n", get_index(x, y, img.line_len));
 	// printf("color%d\n", color);
 	return (color);
 }
-
-void	draw_vertical_line(t_game *game, t_vec low, t_vec high, t_img img)
+void	draw_vertical_line(t_game *game, t_vec low, t_vec high, int color)
 {
 	int	i;
-	int	y;
-	int	distance;
 	int	text;
-	int	color;
+	int	distance;
+	int	y;
 
-	text = 0;
-	i = 0;
-	y = low.y;
 	if (high.x != low.x)
 		return ;
-	if (img.img != game->mlx.frame.img)
+	y = low.y;
+	text = 0;
+	if (color == -1)
 		text = 1;
-	else if (low.y == 0)
-		color = game->config.ceil_color;
-	else
-		color = game->config.floor_color;
+	i = 0;
 	distance = (int)fabs(low.y - high.y);
 	while (i <= distance)
 	{
 		if (text == 1)
-			color = color_texture(img, low.x, i, distance);
+			color = color_texture(game->mlx.tex[game->ray.tex], low.x, i,
+					distance);
 		if (low.x >= 0 && low.x < game->mlx.frame.width && y >= 0
 			&& y < game->mlx.frame.height)
 			game->mlx.frame.addr[get_index(low.x, y,
@@ -84,11 +77,11 @@ void	draw_window(t_game *game, int x, t_vec high_wall, t_vec low_wall)
 	min_win.x = x;
 	min_win.y = 0;
 	if (high_wall.y >= 0 && high_wall.y <= WIN_H / 2)
-		draw_vertical_line(game, min_win, high_wall, game->mlx.frame);
+		draw_vertical_line(game, min_win, high_wall, game->config.ceil_color);
 	// dessiner le ciel
 	if (low_wall.y <= WIN_H && low_wall.y >= WIN_H / 2)
-		draw_vertical_line(game, low_wall, max_win, game->mlx.frame);
+		draw_vertical_line(game, low_wall, max_win, game->config.floor_color);
 	// dessiner le sol
-	draw_vertical_line(game, high_wall, low_wall, game->mlx.tex[game->ray.tex]);
+	draw_vertical_line(game, high_wall, low_wall, -1);
 	// dessiner le mur
 }
